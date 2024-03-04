999affcedadddfcfb6a2e2e31135884da2a4f8b1
Esteban Higuera <esteban3622@gmail.com>
2024-03-03 22:19:42 -0500

    Delete Informática directory

--- Informática/Aprenda JAVA/Ejercicio 1/Geometria.java
+++
@@ -1,7 +0,0 @@
-// fichero Geometria.java
-public abstract class Geometria {
-    // clase abstracta que no puede tener objetos
-    public abstract double perimetro();
-
-    public abstract double area();
-}
